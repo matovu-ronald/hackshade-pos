51b7395658768ddf3ffb2a34497804e57eb95231
Matovu Ronald <maronwhite6@gmail.com>
2019-04-03 16:07:12 +0300

    Apply fixes from StyleCI (#1)
    [ci skip] [skip ci]

--- app/User.php
+++ app/User.php
@@ -3,9 +3,8 @@
 namespace App;
 
 use Backpack\CRUD\CrudTrait;
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
